422f175d6ee6406d7305ba115e7e9a71c55008d4
Maksim Drobotenko <ywit2020@yandex.ru>
2017-01-22 06:04:53 +0700

    sample

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+
+gulp.task('sass', function() {
+  return gulp.src('assets/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+  gulp.watch('assets/*.html', browserSync.reload);
+  gulp.watch('assets/js/**/*.js', browserSync.reload);
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'assets'
+    },
+  })
+})
